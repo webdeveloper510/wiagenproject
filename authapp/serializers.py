9b40b281ff50cbf96cfd59f11b02b215a1b9b68c
root <root@ip-172-31-87-226.ec2.internal>
2023-05-09 12:31:18 +0000

    changes

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -26,23 +26,22 @@ class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model= Content
         fields="__all__"
-        
-
-# class CricketSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model= Cricket_Question_and_Answer
-#         fields = '__all__'
+ 
+class CricketSerializer(serializers.ModelSerializer):
+     class Meta:
+         model= Cricket_Question_and_Answer
+         fields = '__all__'
            
-#     def create(self, validate_data):
-#          return Cricket_Question_and_Answer.objects.create(**validate_data)
+     def create(self, validate_data):
+          return Cricket_Question_and_Answer.objects.create(**validate_data)
      
-# class Mobile_Technology_WavesSerializer(serializers.ModelSerializer):
-#      class Meta:
-#         model= Mobile_Technology_Waves
-#         fields = '__all__'
+class Mobile_Technology_WavesSerializer(serializers.ModelSerializer):
+      class Meta:
+         model= Mobile_Technology_Waves
+         fields = '__all__'
            
-#      def create(self, validate_data):
-#          return Mobile_Technology_Waves.objects.create(**validate_data)
+      def create(self, validate_data):
+          return Mobile_Technology_Waves.objects.create(**validate_data)
 
     
 class TechnologieSerializer(serializers.ModelSerializer):
--- authapp/views.py
+++ authapp/views.py
@@ -115,96 +115,94 @@ class ContenViews(APIView):
             outputs.append(translated_output)
         update_content=Content.objects.filter(id=content_id).update(output=outputs)
         return Response({'msg':'Data Added Succesfully','status':'status.HTTP_201_CREATED','output':outputs})   
-
-# class CricketScrapingView(APIView):
-    # global array
-    # def post(self, request, format=None):
-    #     urls = ["https://www.prep4ias.com/top-300-cricket-general-knowledge-questions-and-answers/","https://www.edubabaji.com/top-50-cricket-gk-questions-answers-in-english/"]
-    #     array=[]
-    #     for index, url in enumerate(urls):
-    #         if index ==0:
+class CricketScrapingView(APIView):
+	def post(self, request, format=None):
+    	     urls = ["https://www.prep4ias.com/top-300-cricket-general-knowledge-questions-and-answers/","https://www.edubabaji.com/top-50-cricket-gk-questions-answers-in-english/"]
+    	     array=[]
+    	     for index, url in enumerate(urls):
+	         if index ==0:
     #                 print(url)
 
-    #                 response = requests.get(url)
-    #                 html_content = response.content
-    #                 soup = BeautifulSoup(html_content, "html.parser")
-    #                 pattern = r'^\d{1,2}\b'
-    #                 h3_tags = soup.find_all("h3")
-    #                 count = 0
-    #                 for h3 in h3_tags:
-    #                     if count == 60:
-    #                         break
-    #                     question=h3.text
-    #                     question=re.sub(r'\.', '', question)
-    #                     question=re.sub(r'\d+', '', question)
+	                 response = requests.get(url)
+	                 html_content = response.content
+    	                 soup = BeautifulSoup(html_content, "html.parser")
+	                 pattern = r'^\d{1,2}\b'
+    	                 h3_tags = soup.find_all("h3")
+    	                 count = 0
+    	                 for h3 in h3_tags:
+   	                     if count == 60:
+    	                         break
+    	                     question=h3.text
+   	                     question=re.sub(r'\.', '', question)
+    	                     question=re.sub(r'\d+', '', question)
     #                     # print(question)
-    #                     answer=h3.find_next("p").text
-    #                     answer=re.sub(r'\.', '', answer)
-    #                     answer= re.sub(r'\bAns\b', '', answer)
-    #                     data_dict={"question":question,"answer":answer}
-    #                     array.append(data_dict)
+    	                     answer=h3.find_next("p").text
+   	                     answer=re.sub(r'\.', '', answer)
+    	                     answer= re.sub(r'\bAns\b', '', answer)
+    	                     data_dict={"question":question,"answer":answer}
+    	                     array.append(data_dict)
     #                     # print(array)
-    #                     count += 1
-    #         if index ==1:
-    #                 print(url)
-    #                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    	                     count += 1
+             if index ==1:
+    	#                 print(url)
+                     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
-    #                 response = requests.get(url,headers=headers)
+                     response = requests.get(url,headers=headers)
 
-    #                 soup = BeautifulSoup(response.content, "html.parser")
+                     soup = BeautifulSoup(response.content, "html.parser")
 
-    #                 video_wrapper = soup.find("div", {"class": "jetpack-video-wrapper"})
+                     video_wrapper = soup.find("div", {"class": "jetpack-video-wrapper"})
 
-    #                 ol_tag = soup.find('ol')
+                     ol_tag = soup.find('ol')
 
-    #                 for li_tag in ol_tag.find_all('li'):
-    #                     li_text = li_tag.text.strip()
-    #                     strong_tag = li_tag.find('strong')
-    #                     if strong_tag:
-    #                         strong_text = strong_tag.text.strip()
-    #                     else:
-    #                         strong_text = None
+                     for li_tag in ol_tag.find_all('li'):
+                         li_text = li_tag.text.strip()
+                         strong_tag = li_tag.find('strong')
+                         if strong_tag:
+                             strong_text = strong_tag.text.strip()
+                         else:
+                             strong_text = None
                         
-    #                     question=li_text
-    #                     question=question.split('?')[0] + '?'
-    #                     # print(question)
+                         question=li_text
+                         question=question.split('?')[0] + '?'
+                         # print(question)
 
-    #                     answer=strong_text
-    #                     answer=answer[1:]
-    #                     # print(answer)
-    #                     data_dict2={"question":question,"answer":answer}
-    #                     array.append(data_dict2)
-    #     for x in array:
+                         answer=strong_text
+                         answer=answer[1:]
+                         # print(answer)
+                         data_dict2={"question":question,"answer":answer}
+                         array.append(data_dict2)
+         for x in array:
     #         print(x)
-    #         question=(x['question'])
-    #         answer=(x['answer'])
-    #         cricketdata=Cricket_Question_and_Answer.objects.create(question=question,answer=answer)
-    #         # serializer=CricketSerializer(data=cricketdata)
-    #         cricketdata.save()
-    #     return Response({"message":"scrap data successfully","status":"200","data":"ok"})
+             question=(x['question'])
+             answer=(x['answer'])
+             cricketdata=Cricket_Question_and_Answer.objects.create(question=question,answer=answer)
+             serializer=CricketSerializer(data=cricketdata)
+             cricketdata.save()
+         return Response({"message":"scrap data successfully","status":"200","data":"ok"})
     
-# class WebScrapDataView(APIView):
-#      def get(self, request, format=None):
-#         page = requests.get("https://buildfire.com/mobile-technology-waves/")
-#         soup = BeautifulSoup(page.content, "html.parser")
-#         p_tags = soup.select("h2 + p")
-#         array=[]
-#         count=0
-#         pattern = r'\d+'
-#         for p_tag in p_tags:
-#             h2_tag = p_tag.previous_sibling.previous_sibling
-#             if h2_tag is not None and h2_tag.name == "h2":
-#                 print(h2_tag.text)
-#             print(p_tag.text)
-#             question = re.sub(pattern, '', h2_tag.text)
-#             answer=re.sub(pattern, '', p_tag.text)
-#             scrappy=Mobile_Technology_Waves.objects.create(question=question,answer=answer)
-#             serializers=Mobile_Technology_WavesSerializer(data=scrappy)
-#             scrappy.save()
-#             count+=1
-#             if count==23:
-#                 break
-#         return Response({"message":"scrap data successfully","status":"200","Data":array})
+class WebScrapDataView(APIView):
+      def get(self, request, format=None):
+         page = requests.get("https://buildfire.com/mobile-technology-waves/")
+         soup = BeautifulSoup(page.content, "html.parser")
+         p_tags = soup.select("h2 + p")
+         array=[]
+         count=0
+         pattern = r'\d+'
+         for p_tag in p_tags:
+             h2_tag = p_tag.previous_sibling.previous_sibling
+             if h2_tag is not None and h2_tag.name == "h2":
+                 print(h2_tag.text)
+             print(p_tag.text)
+             question = re.sub(pattern, '', h2_tag.text)
+             answer=re.sub(pattern, '', p_tag.text)
+             scrappy=Mobile_Technology_Waves.objects.create(question=question,answer=answer)
+             serializers=Mobile_Technology_WavesSerializer(data=scrappy)
+             scrappy.save()
+             count+=1
+             if count==23:
+                 break
+         return Response({"message":"scrap data successfully","status":"200","Data":array})
 
 
 class TechnologyView(APIView):
